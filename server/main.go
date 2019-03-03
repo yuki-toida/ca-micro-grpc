7e50ee8e7c813f7928e0be0757688a86ae008b1b
YukiToida <yuki.toidax@gmail.com>
2019-03-03 10:21:16 +0900

    add proto

---
+++ server/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"net"
+
+	"github.com/yuki-toida/grpc-clean/server/pb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		panic(err)
+	}
+	server := grpc.NewServer()
+	pb.RegisterTestServer(server, &Server{})
+	server.Serve(listener)
+}
+
+type Server struct{}
+
+func (s *Server) Get(c context.Context, r *pb.Request) (*pb.Response, error) {
+	return &pb.Response{Message: r.Message}, nil
+}
